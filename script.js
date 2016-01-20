0423deff6a5cde4d71294d055e43948d697dc6d9
satvik <satvik.jagannath7@gmail.com>
2016-01-21 01:06:19 +0530

    XMLhttprequest

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-for(i=1; i<=2;i++)
-{
-  var division = "http://hitwicket.com/league/show/v-"+i;
-$.get( division, function( resp ) {
-//  if($('div').hasClass('.redColor')){
-    //var success =  $($.parseHTML(resp)).filter(".bot_team_name").length; 
-  var success =  $(resp).hasClass(".bot_team_name"); 
-    //var numItems = $('.bot_team_name').length;
-    i;
-    success;
-});
+function leagueurlgen (division) {
+	for(i=1; i<=2;i++)
+	{
+	  getDataFromHitwicket("http://hitwicket.com/league/show/v-"+i);
+	}
+}
+
+function getDataFromHitwicket (leagueurl) {	
+  var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", leagueurl, true);
+xmlhttp.send();
+}
+
+function myFunction(arr) {
+    // var out = "";
+    // var i;
+    // for(i = 0; i < arr.length; i++) {
+    //     out += '<a href="' + arr[i].url + '">' + 
+    //     arr[i].display + '</a><br>';
+    // }
+    document.getElementById("id01").innerHTML = arr;
 }
 
 //var numItems = $('.bot_team_name').length;
